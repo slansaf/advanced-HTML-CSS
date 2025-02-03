d56f470dfc30bd866c4b27be280a80eef6e16d63
SlanSaf <slansaf@yandex.ru>
2025-02-03 21:10:03 +0300

    Добавлен функционал добавления товаров в корзину JavaScript

--- script/script-cart.js
+++ script/script-cart.js
@@ -1,73 +1,132 @@
-const url = './script/data.json';
+function displayCart() {
+	const cart = JSON.parse(localStorage.getItem('cart')) || [];
+	const cartList = document.querySelector('.cart__cards');
+	cartList.innerHTML = ''; 
 
-async function getData(url) {
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error.message);
+	if (cart.length === 0) {
+		cartList.innerHTML = '<p>Корзина пуста</p>';
+		return;
 	}
-}
 
-document.addEventListener('DOMContentLoaded', async (e) => {
-	const data = await getData(url);
-	const list = document.querySelector('.cart__cards');
-	data.forEach(element => {
-		list.insertAdjacentHTML('afterbegin', `
-			<div class="cart__card">
-						<img
-							class="cart__img"
-							src=${element.img}
-							alt="man-in-cart"
-						/>
-						<div class="cart__price">
-							<a href="#" class="cart__head"> ${element.title} </a>
-							<ul class="cart__ul">
-								<li>Price: <span class="cart__ul_color">$${element.price}</span></li>
-								<li>Color: ${element.color}</li>
-								<li>Size: ${element.size}</li>
-								<li>
+	cart.forEach(element => {
+		cartList.insertAdjacentHTML('afterbegin', `
+			<div class="cart__card" data-id="${element.id}"> <!-- Добавлен атрибут data-id -->
+					<img class="cart__img" src="${element.img}" alt="man-in-cart" />
+					<div class="cart__price">
+						<a href="#" class="cart__head"> ${element.title} </a>
+						<ul class="cart__ul">
+							<li>Price: <span class="cart__ul_color">${element.price}</span></li>
+							<li>Color: ${element.color}</li>
+							<li>Size: ${element.size}</li>
+							<li>
 									Quantity:
-									<input
-										type="number"
-										value=${element.count}
-										class="cart__ul_quantity"
-									/>
-								</li>
-							</ul>
-							<a href="#" class="cart__cross">
-								<svg
-									width="18.000000"
-									height="18.000000"
-									viewBox="0 0 18 18"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-									xmlns:xlink="http://www.w3.org/1999/xlink"
-								>
+									<input type="number" value="${element.count}" class="cart__ul_quantity" />
+							</li>
+						</ul>
+						<a href="#" class="cart__cross">
+							<svg width="18.000000" height="18.000000" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<desc>Created with Pixso.</desc>
 									<defs />
-									<path
-										id="Vector"
-										d="M11.24 9L17.53 2.71C17.82 2.41 17.99 2.01 17.99 1.59C17.99 1.17 17.82 0.76 17.53 0.46C17.23 0.16 16.83 0 16.4 0C15.98 0 15.58 0.16 15.28 0.46L9 6.75L2.71 0.46C2.41 0.16 2.01 0 1.59 0C1.16 0 0.76 0.16 0.46 0.46C0.16 0.76 0 1.16 0 1.59C0 2.01 0.16 2.41 0.46 2.71L6.75 9L0.46 15.28C0.16 15.58 0 15.98 0 16.4C0 16.83 0.16 17.23 0.46 17.53C0.76 17.83 1.16 18 1.59 18C2.01 18 2.41 17.83 2.71 17.53L9 11.24L15.28 17.53C15.58 17.83 15.98 18 16.4 18C16.83 18 17.23 17.83 17.53 17.53C17.83 17.23 18 16.83 18 16.4C18 15.98 17.83 15.58 17.53 15.28L11.24 9Z"
-										fill="#575757"
-										fill-opacity="1.000000"
-										fill-rule="nonzero"
-									/>
-								</svg>
-							</a>
-						</div>
+									<path id="Vector" d="M11.24 9L17.53 2.71C17.82 2.41 17.99 2.01 17.99 1.59C17.99 1.17 17.82 0.76 17.53 0.46C17.23 0.16 16.83 0 16.4 0C15.98 0 15.58 0.16 15.28 0.46L9 6.75L2.71 0.46C2.41 0.16 2.01 0 1.59 0C1.16 0 0.76 0.16 0.46 0.46C0.16 0.76 0 1.16 0 1.59C0 2.01 0.16 2.41 0.46 2.71L6.75 9L0.46 15.28C0.16 15.58 0 15.98 0 16.4C0 16.83 0.16 17.23 0.46 17.53C0.76 17.83 1.16 18 1.59 18C2.01 18 2.41 17.83 2.71 17.53L9 11.24L15.28 17.53C15.58 17.83 15.98 18 16.4 18C16.83 18 17.23 17.83 17.53 17.53C17.83 17.23 18 16.83 18 16.4C18 15.98 17.83 15.58 17.53 15.28L11.24 9Z" fill="#575757" fill-opacity="1.000000" fill-rule="nonzero" />
+							</svg>
+						</a>
 					</div>
-			`)
+			</div>
+		`);
 	});
-	
-	list.addEventListener('click', (e) => {
-		if(e.target.closest('.cart__cross')){
+
+	cartList.addEventListener('click', (e) => {
+		if (e.target.closest('.cart__cross')) {
 			const item = e.target.closest('.cart__card');
-			if (item) {
-				item.remove();
-			}
+			const productId = parseInt(item.getAttribute('data-id'), 10); // Получаем ID товара
+			removeFromCart(productId); // Передаем ID в функцию удаления
 		}
 	});
-});
+}
+
+function removeFromCart(productId) {
+	// Получаем текущую корзину из localStorage
+	let cart = JSON.parse(localStorage.getItem('cart')) || [];
+	// Фильтруем корзину, удаляя товар с указанным ID
+	cart = cart.filter(item => item.id !== productId);
+	// Сохраняем обновленную корзину в localStorage
+	localStorage.setItem('cart', JSON.stringify(cart));
+	// Обновляем отображение корзины
+	displayCart();
+}
+
+document.addEventListener('DOMContentLoaded', displayCart);
+
+// const url = './script/data.json';
+
+// async function getData(url) {
+// 	try {
+// 		const response = await fetch(url);
+// 		const data = await response.json();
+// 		return data;
+// 	} catch (error) {
+// 		console.log(error.message);
+// 	}
+// }
+
+// document.addEventListener('DOMContentLoaded', async (e) => {
+// 	const data = await getData(url);
+// 	const list = document.querySelector('.cart__cards');
+// 	data.forEach(element => {
+		// list.insertAdjacentHTML('afterbegin', `
+		// 	<div class="cart__card">
+		// 				<img
+		// 					class="cart__img"
+		// 					src=${element.img}
+		// 					alt="man-in-cart"
+		// 				/>
+		// 				<div class="cart__price">
+		// 					<a href="#" class="cart__head"> ${element.title} </a>
+		// 					<ul class="cart__ul">
+		// 						<li>Price: <span class="cart__ul_color">$${element.price}</span></li>
+		// 						<li>Color: ${element.color}</li>
+		// 						<li>Size: ${element.size}</li>
+		// 						<li>
+		// 							Quantity:
+		// 							<input
+		// 								type="number"
+		// 								value=${element.count}
+		// 								class="cart__ul_quantity"
+		// 							/>
+		// 						</li>
+		// 					</ul>
+		// 					<a href="#" class="cart__cross">
+		// 						<svg
+		// 							width="18.000000"
+		// 							height="18.000000"
+		// 							viewBox="0 0 18 18"
+		// 							fill="none"
+		// 							xmlns="http://www.w3.org/2000/svg"
+		// 							xmlns:xlink="http://www.w3.org/1999/xlink"
+		// 						>
+		// 							<desc>Created with Pixso.</desc>
+		// 							<defs />
+		// 							<path
+		// 								id="Vector"
+		// 								d="M11.24 9L17.53 2.71C17.82 2.41 17.99 2.01 17.99 1.59C17.99 1.17 17.82 0.76 17.53 0.46C17.23 0.16 16.83 0 16.4 0C15.98 0 15.58 0.16 15.28 0.46L9 6.75L2.71 0.46C2.41 0.16 2.01 0 1.59 0C1.16 0 0.76 0.16 0.46 0.46C0.16 0.76 0 1.16 0 1.59C0 2.01 0.16 2.41 0.46 2.71L6.75 9L0.46 15.28C0.16 15.58 0 15.98 0 16.4C0 16.83 0.16 17.23 0.46 17.53C0.76 17.83 1.16 18 1.59 18C2.01 18 2.41 17.83 2.71 17.53L9 11.24L15.28 17.53C15.58 17.83 15.98 18 16.4 18C16.83 18 17.23 17.83 17.53 17.53C17.83 17.23 18 16.83 18 16.4C18 15.98 17.83 15.58 17.53 15.28L11.24 9Z"
+		// 								fill="#575757"
+		// 								fill-opacity="1.000000"
+		// 								fill-rule="nonzero"
+		// 							/>
+		// 						</svg>
+		// 					</a>
+		// 				</div>
+		// 			</div>
+		// 	`)
+// 	});
+	
+	// list.addEventListener('click', (e) => {
+	// 	if(e.target.closest('.cart__cross')){
+	// 		const item = e.target.closest('.cart__card');
+	// 		if (item) {
+	// 			item.remove();
+	// 		}
+	// 	}
+	// });
+// });
 
--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const url = '../script/data-index.json';
+let data = []; 
 
 async function getData(url) {
 	try {
@@ -10,16 +11,14 @@ async function getData(url) {
 	}
 }
 
-
-document.addEventListener('DOMContentLoaded', async (e) => {
-	const data = await getData(url);
+function addProductsToList(products){
 	const list = document.querySelector('.fetured_items__grid');
-	data.forEach(element => {
+	products.forEach(element => {
 		list.insertAdjacentHTML('beforeend', `
 				<div class="item${element.id} items">
 					<div class="items__img">
 						<img src="${element.img}" alt="" />
-						<button class="hiden_button">Add to Cart</button>
+						<button class="hiden_button" data-id="${element.id}">Add to Cart</button>
 					</div>
 
 					<div class="card_items">
@@ -32,6 +31,49 @@ document.addEventListener('DOMContentLoaded', async (e) => {
 				</div>
 			`)
 	});
+
+}
+
+function addToCart(productId) {
+	   // Получаем данные о продукте по его ID
+		const product = data.find(item => item.id === productId);
+		console.log(productId);
+		if (product) {
+			 // Получаем текущую корзину из localStorage
+			let cart = JSON.parse(localStorage.getItem('cart')) || [];
+			// Проверяем, есть ли уже этот продукт в корзине
+			const existingProductIndex = cart.findIndex(item => item.id === productId);
+			if (existingProductIndex > -1) {
+			  // Если продукт уже есть, увеличиваем количество
+				cart[existingProductIndex].quantity += 1;
+			} else {
+			  // Если продукта нет, добавляем его в корзину с количеством 1
+			  product.quantity = 1; // Добавляем свойство quantity
+				cart.push(product);
+			}
+			
+			// Сохраняем обновленную корзину в localStorage
+			localStorage.setItem('cart', JSON.stringify(cart));
+			alert('Товар добавлен в корзину!');
+		} else {
+			console.log('Product not found');
+		}
+	}
+
+
+
+// Обработчик клика на кнопки "Add to Cart"
+document.addEventListener('click', (event) => {
+	if (event.target && 	event.target.classList.contains('hiden_button')) {
+	const productId =	parseInt(event.target.getAttribute('data-id'), 10);
+	addToCart(productId);
+	}
+});
+
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+	data = await getData(url);
+	addProductsToList(data);
 });
 
 
